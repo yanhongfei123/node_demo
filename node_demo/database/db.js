65baa8714a784766a82fef8dde18def0ee4983b9
yanhongfei123 <970733805@qq.com>
2019-04-29 14:29:33 +0800

    Update db.js

--- node_demo/database/db.js
+++ node_demo/database/db.js
@@ -1,10 +1,10 @@
 
-var  chalk = require('chalk')
+var chalk = require('chalk')
 var mongoose = require("mongoose");
 // 连接数据库 ,数据库的名称可以是不存在 创建一个 datadbbase 数据库
 //mongoose.connect("mongodb://yanhongfei:yhf199012@127.0.0.1:27017/yhf_db");
-mongoose.connect("mongodb://yanhongfei:199012@127.0.0.1:27017/datadbbase");
 
+mongoose.connect("mongodb://yanhongfei:199012@127.0.0.1:27017/datadbbase");
 mongoose.connection.on("error", function(err){
     console.log(
         chalk.red('数据库链接失败:' + err)
